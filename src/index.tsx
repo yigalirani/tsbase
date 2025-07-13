1623d9b932b52ff5abcbf6a13b1bef3e8392a0ae
Yigal Irani <yigal@symbolclick.com>
2025-07-13 19:45:52 -0400

    react query demo works!1

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,63 @@
-import * as React from 'react'
+import {useState} from  'react'
 import ReactDOM from 'react-dom/client'
-function App(){
-          const [count, setCount] = React.useState(0);
+
+
+
+
+function AppCount(){
+          const [count, setCount] = useState(0);
 
   return <div>
       <h1 onMouseDown={() => setCount(count + 1)}>Count: {count}</h1>
     </div>
 
 }
+
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
+
+
+
+function Example() {
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: async () => {
+      const response = await fetch(
+        'https://api.github.com/repos/TanStack/query',
+      )
+      return await response.json()
+    },
+  })
+
+  if (isPending) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+
+  return (
+    <div>
+      <h1>{data.full_name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{' '}
+      <strong>✨ {data.stargazers_count}</strong>{' '}
+      <strong>🍴 {data.forks_count}</strong>
+      <div>{isFetching ? 'Updating...' : ''}</div>
+    </div>
+  )
+}
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <Example />
+    </QueryClientProvider>
+  )
+}
+
 const rootElement = document.getElementById('root') as HTMLElement
 ReactDOM.createRoot(rootElement).render(<App />)
