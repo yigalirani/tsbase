90e543561e1fdb4eb68a0983b9736a9282784302
Yigal Irani <yigal@symbolclick.com>
2025-07-13 17:44:11 -0400

    colorfull eslint

--- run_eslint.mjs
+++ run_eslint.mjs
@@ -1,6 +1,6 @@
 const { exec } = await import('node:child_process');
 
-exec('npx eslint . --format visualstudio',(error, stdout, stderr) => {
+exec('npx eslint .  --color',(error, stdout, stderr) => {
   console.log(`stdout: ${stdout}`);
   console.error(`stderr: ${stderr}`);
 });
