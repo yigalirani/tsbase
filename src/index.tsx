de8e3db328dcd2ae97e16acc955d9282509a9806
Yigal Irani <yigal@symbolclick.com>
2025-07-25 11:15:11 -0400

    using more features of react query

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
-import {useState} from  'react'
+import {useState,useEffect} from  'react'
 import ReactDOM from 'react-dom/client'
-
-
-
 
 function AppCount(){
           const [count, setCount] = useState(0);
@@ -22,31 +19,72 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
+function Timer({ startMs }: { startMs: number }) {
+  const [diff, setDiff] = useState(0);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDiff(Date.now() - startMs);
+    }, 10); // update every 10ms
 
+    return () => clearInterval(interval);
+  }, [startMs]);
+
+  return (
+    <div>
+      <h1>Elapsed Time</h1>
+      <p>{diff} ms</p>
+    </div>
+  );
+}
+const urls=[
+  'https://api.github.com/repos/TanStack/query',
+  'https://api.github.com/repos/yigalirani/homelayer'
+
+]
+interface SelectUrlProps {
+  set_url: (url: string) => void;
+}
+
+function SelectUrl({ set_url }: SelectUrlProps) {
+  return (
+    <select onChange={(e) => set_url(e.target.value)}>
+      {urls.map((url, index) => (
+        <option key={index} value={url}>
+          {url}
+        </option>
+      ))}
+    </select>
+  );
+}
 function Example() {
-  const { isPending, error, data, isFetching } = useQuery({
-    queryKey: ['repoData'],
+  const [url,set_url]=useState(urls[0])
+  const select=SelectUrl({set_url})
+  const ret = useQuery({
+    queryKey: ['repoData',url],
     queryFn: async () => {
-      const response = await fetch(
-        'https://api.github.com/repos/TanStack/query',
-      )
+      const response = await fetch(url)
+      await new Promise((resolve) => setTimeout(resolve, 1000))
       return await response.json()
     },
   })
-
-  if (isPending) return 'Loading...'
+  console.log({ret})
+  const{ isPending, error, data, isFetching }=ret
+  if (isPending) return <div>{select}'Loading...'</div>
 
   if (error) return 'An error has occurred: ' + error.message
 
   return (
     <div>
+      <button onClick={()=>queryClient.invalidateQueries()}>invlidate</button>
+      {select}
       <h1>{data.full_name}</h1>
       <p>{data.description}</p>
       <strong>👀 {data.subscribers_count}</strong>{' '}
       <strong>✨ {data.stargazers_count}</strong>{' '}
       <strong>🍴 {data.forks_count}</strong>
       <div>{isFetching ? 'Updating...' : ''}</div>
+      <pre>{JSON.stringify(ret,null,2)}</pre>      
     </div>
   )
 }
