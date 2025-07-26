f49891abda5e354d82568a381dd0192bcf9171f5
Yigal Irani <yigal@symbolclick.com>
2025-07-26 18:29:28 -0400

    added row for another react demo

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import {useState,useEffect} from  'react'
+import React,{useState,useEffect} from  'react'
 import ReactDOM from 'react-dom/client'
-
+import { ReactQueryDevtoolsPanel } from '@tanstack/react-query-devtools'
 function AppCount(){
           const [count, setCount] = useState(0);
 
@@ -16,7 +16,19 @@ import {
   useQuery,
 } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+function Devtools() {
+  const [isOpen, setIsOpen] = React.useState(false)
 
+  return (
+    <QueryClientProvider client={queryClient}>
+      {/* The rest of your application */}
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+      >{`${isOpen ? 'Close' : 'Open'} the devtools panel`}</button>
+      {isOpen && <ReactQueryDevtoolsPanel onClose={() => setIsOpen(false)} />}
+    </QueryClientProvider>
+  )
+}
 const queryClient = new QueryClient()
 
 function Timer({ startMs }: { startMs: number }) {
@@ -57,9 +69,14 @@ function SelectUrl({ set_url }: SelectUrlProps) {
     </select>
   );
 }
+function reorder(a:object){
+  const {data}=a
+  return [{...a,data:''},data]
+}
 function Example() {
   const [url,set_url]=useState(urls[0])
   const select=SelectUrl({set_url})
+  
   const ret = useQuery({
     queryKey: ['repoData',url],
     queryFn: async () => {
@@ -67,32 +84,45 @@ function Example() {
       await new Promise((resolve) => setTimeout(resolve, 1000))
       return await response.json()
     },
+    staleTime:4000
   })
   console.log({ret})
-  const{ isPending, error, data, isFetching }=ret
-  if (isPending) return <div>{select}'Loading...'</div>
+  const{ isPending, error, data, isFetching,isStale }=ret
+  if (isPending) return <div>{select}'Loading...'<Devtools/></div>
 
   if (error) return 'An error has occurred: ' + error.message
-
+  function invalidate_it(){
+    queryClient.invalidateQueries()
+  }
   return (
     <div>
-      <button onClick={()=>queryClient.invalidateQueries()}>invlidate</button>
+      <button onClick={invalidate_it}>invlidate</button>
       {select}
+      <Devtools/>
       <h1>{data.full_name}</h1>
       <p>{data.description}</p>
       <strong>👀 {data.subscribers_count}</strong>{' '}
       <strong>✨ {data.stargazers_count}</strong>{' '}
       <strong>🍴 {data.forks_count}</strong>
+            <div>isStale:{isStale+''}</div>
       <div>{isFetching ? 'Updating...' : ''}</div>
-      <pre>{JSON.stringify(ret,null,2)}</pre>      
+      <pre>{JSON.stringify(reorder(ret),null,2)}</pre>      
     </div>
   )
 }
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools />
-      <Example />
+      <div className='splitter'>
+        <div>
+          <ReactQueryDevtools />
+          <Example />
+        </div>
+        <div>
+          this is the right panel
+        </div>          
+      </div>
+    
     </QueryClientProvider>
   )
 }
