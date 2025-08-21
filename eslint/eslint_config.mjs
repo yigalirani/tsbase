a986dc86177e23a6edeb213960ffb9bbb3dbe600
Yigal Irani <yigal@symbolclick.com>
2025-08-21 18:21:03 -0400

    more eslint refining, deleted unneeded files

--- eslint/eslint_config.mjs
+++ eslint/eslint_config.mjs
@@ -5,15 +5,15 @@ import globals from 'globals';
 import { defineConfig,globalIgnores  } from "eslint/config";
 export default defineConfig(
   globalIgnores(["dist/", "types/"]),
-  eslint.configs.all, //this must be adter tseslint.configs or there will be less rules
-  tseslint.configs.strict, //trying all, doesnt work
+  eslint.configs.all, //taking all rules from eslint, truning select ones off below
+  tseslint.configs.strict, 
   {
     plugins: {
       'import-x': importX,
     },
     rules: {
-      //'import-x/no-cycle': 'error',
-      "@typescript-eslint/no-use-before-define":"warn",
+      //'import-x/no-cycle': 'error', //commented out because slow, turn on when needed
+      "@typescript-eslint/no-use-before-define":"warn", //turning this one on because i love it
       "no-inline-comments":"off",
       "sort-keys":"off",
       "func-style":"off",
